5dfc4fe6d205d9e472c53b89c99342f2197054cf
aniu <aniu_98@163.com>
2021-03-03 21:42:59 +0800

    ocr

--- GameAssist/animalMeal/ad.py
+++ GameAssist/animalMeal/ad.py
@@ -1,41 +1,46 @@
 # -*- coding:utf-8 -*-
-from pynput.mouse import Button,Controller
+from pynput.mouse import Button, Controller
 import time
 
-def click(x,y,delay,num=2):
-	mouse= Controller()
-	mouse.position=(x,y)
-	for i in range(1,num):
-		time.sleep(delay)
-		mouse.position=(x,y)
-		mouse.click(Button.left,1)
+
+def click(x, y, delay, num=2):
+    mouse = Controller()
+    mouse.position = (x, y)
+    for i in range(1, num):
+        time.sleep(delay)
+        mouse.position = (x, y)
+        mouse.click(Button.left, 1)
+
+
 def main():
-	for x in range(0,110):
-		print("start click ad %s "%x)
-		# # 升级动物
-		# click(1666,696,2)
-		
-		# 调鱼
-		# click(1770,453,2)
+    for x in range(0, 110):
+        print("start click ad %s " % x)
+        # # 升级动物
+        # click(1666,696,2)
 
-		# 宣传
-		if x==0:
-			click(1791, 790, 2)
-		else:
-			click(1791, 790, 50)
-		
-		click(1695,550,2)
+        # 调鱼
+        # click(1770,453,2)
 
-		# 许愿池
-		# time.sleep(1.5)
-		# click(1873,760,0.08,100*4)
-		# click(1700,624,1)
-		# click(1693,561,1.5)
-		
-		# mute
-		click(1839,77,2)
-		print("close ad after 30s %s"% time.ctime(time.time()))
-		click(1882,77,32)
+        # 宣传
+        if x == 0:
+            click(1791, 790, 2)
+        else:
+            click(1791, 790, 50)
+
+        click(1695, 550, 2)
+
+        # 许愿池
+        # time.sleep(1.5)
+        # click(1873,760,0.08,100*4)
+        # click(1700,624,1)
+        # click(1693,561,1.5)
+
+        # mute
+        click(1839, 77, 2)
+        print("close ad after 30s %s" % time.ctime(time.time()))
+        click(1882, 77, 32)
+
+
 if __name__ == '__main__':
-	main()
-	print("---------end--------------")
+    main()
+    print("---------end--------------")
