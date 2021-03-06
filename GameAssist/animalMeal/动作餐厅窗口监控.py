56795195a49c34b1581af0395a9ce29896aeb132
aniu <aniu_98@163.com>
2021-03-06 18:39:21 +0800

    add myutil

--- GameAssist/animalMeal/动作餐厅窗口监控.py
+++ GameAssist/animalMeal/动作餐厅窗口监控.py
@@ -1,17 +1,18 @@
 # coding=gbk
 import numpy as np
-import cv2
+from cv2 import cv2
 from PIL import ImageGrab
 # pip install pywin32
-from win32gui import FindWindow, GetWindowRect
+from win32 import win32gui
+# from win32gui import FindWindow, GetWindowRect
 
 
 win32gui.GetForegroundWindow()
 
 while True:
     window_name = "动物餐厅"
-    id = FindWindow(None, window_name)
-    bbox = GetWindowRect(id)
+    id = win32gui.FindWindow(None, window_name)
+    bbox = win32gui.GetWindowRect(id)
     image_array = np.array(ImageGrab.grab(bbox=bbox))
     image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
     cv2.imshow('screenshot',image_array)
