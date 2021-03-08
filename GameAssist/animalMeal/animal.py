f6d35c0f0d933411516d0938818891a9cd2e4c7c
aniu <aniu_98@163.com>
2021-03-09 00:02:38 +0800

    add  esc exit the pro

--- GameAssist/animalMeal/animal.py
+++ GameAssist/animalMeal/animal.py
@@ -82,7 +82,7 @@ def runJob(threadName, delay, counter):
         
 
         # 手机宣传
-        click(threadName,0.08,1873,760,29*15)
+        click(threadName,0.08,1873,760,28*10)
         # click(threadName,0.03,289,572,32*5)\
         count=count+1
         print(count)
